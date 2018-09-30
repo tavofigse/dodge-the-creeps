7a698df4746f5e76030e6b9ab0f442acbc394318
Gustavo <gfasequeira@gmail.com>
2018-09-30 11:30:08 -0300

    Add music

--- Main.gd
+++ Main.gd
@@ -10,6 +10,8 @@ func game_over():
     $ScoreTimer.stop()
     $MobTimer.stop()
     $HUD.show_game_over()
+    $Music.stop()
+    $DeathSound.play()
 
 func new_game():
     score = 0
@@ -17,6 +19,7 @@ func new_game():
     $StartTimer.start()
     $HUD.update_score(score)
     $HUD.show_message("Get Ready")
+    $Music.play()
 
 func _on_StartTimer_timeout():
     $MobTimer.start()
